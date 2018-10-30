90ca0c85744a1579cd63ae8214a023467d4ec085
Ольга Боровикова <olchick.borovikova@yandex.ru>
2018-10-30 14:44:31 +0300

    Update hw_4.hs

--- hw_4.hs
+++ hw_4.hs
@@ -32,18 +32,17 @@ minMax :: Ord a => [a] -> Maybe (a, a) -- (min, max)
 -- функция для min
 min_ :: (Ord a) => a->a->a
 min_ x y = if x<= y then x else y
-minimum_ :: (Ord a) => [a] -> a
-minimum_ [x] = x
-minimum_ (x:xs) = min_ x (minimum_ xs)
 -- функция для max
 max_ :: (Ord a) => a->a->a
 max_ x y = if x>=y then x else y
-maximum_ :: (Ord a) => [a] -> a
-maximum_ [x] = x
-maximum_ (x:xs) = max_ x (maximum_ xs)
+
+min_max :: (Ord a) => (a->a->a)->[a] -> a
+min_max func [x] = x
+min_max func (x:xs) = func x (min_max func xs)
+
 -- Определние для общей функции minMax
 minMax [] = Nothing
-minMax (x:xs) = Just(minimum_ (x:xs), maximum_ (x:xs))
+minMax (x:xs) = Just(min_max min_ (x:xs), min_max max_ (x:xs))
 
 -- 3 -------------------------------
 -- Реализуйте функции, находящие сумму и количество цифр(в 10-ой системе счисления)
@@ -53,12 +52,12 @@ minMax (x:xs) = Just(minimum_ (x:xs), maximum_ (x:xs))
 sum_and_count :: Integer -> (Integer, Integer)
 sum_and_count x | x==0 = (0,1)
                         | otherwise = (sum n, toInteger (length n))
-                                   where n = list x
+                                   where n = list (x, 0) 0
 -- функция разбора числа для 10-ой системы счисления
-list :: Integer -> [Integer]
-list 0 = []
-list x | x<0 = list (-x)
-       | otherwise = list (x `div` 10) ++ [x `mod` 10]
+list :: (Integer, Integer) -> Integer -> [Integer]
+list (0, y) k = [y]
+list (x, y) k | x<0 = list (divMod (-x) 10) (k+1)
+       | otherwise = if k==0 then (list (divMod x 10) (k+1)) else (list (divMod x 10) k ) ++ [y]
 
 -- 4 --------------------------------
 
@@ -67,17 +66,18 @@ list x | x<0 = list (-x)
 -- длины n называется преобладающим, если он встречается в нем более чем n/2 раз.
 -- Найдите решение с линейным временем работы — O(n).
 -- функция - алгоритм
-funct :: Char -> Integer -> [Char] -> Char
+funct :: (Eq a) => a -> Integer -> [a] -> a
+funct s c [] = s
 funct s c (x:xs) | c==0 = funct x (c+1) xs
                  | otherwise = if s==x then funct s (c+1) xs else funct s (c-1) xs
-funct s c [] = s
 -- функции для проверки результата - поиск утечки
-coun_t :: [Char]->Int
-coun_t xs = length $ filter (==(funct ' ' 0 xs)) xs
+coun_t :: (Eq a) => [a]->Int
+coun_t xs = length $ filter (==(funct (head xs) 0 xs)) xs
 -- основная функция 
-sho_w :: [Char]->[Char]
-sho_w xs = if ((coun_t xs)>((length xs) `div` 2)) then [funct ' ' 0 xs] else "no simbols"
--- в результате по списку проходмся мах 3 раза (3n). Сам алгоритм требует одно прохождение -- по списку, но так как существуют проверки число прохождений увеличивается до 3 раз
+sho_w :: (Eq a) => [a]-> Maybe a
+sho_w xs = if ((coun_t xs)>((length xs) `div` 2)) then Just(funct (head xs) 0 xs) else Nothing
+-- в результате по списку проходмся мах 3 раза (3n). Сам алгоритм требует одно прохождение 
+-- по списку, но так как существуют проверки число прохождений увеличивается до 3 раз
 
 -- 5 ---------------------------------
 f:: (a -> a) -> Int -> (a -> a)
@@ -92,11 +92,13 @@ f g n | n==0 = g
 -- Основная функция 
 last_fibb :: Integer -> Integer
 last_fibb y = last(list z)
-                 where z = fibb y
+                where z = fibb (0,0,1) y
 -- функция list описана выше 
 -- функция нахождения n-го члена последовательности Фибоначчи
-fibb :: Integer -> Integer
-fibb x = if (x==0 || x==1) then x else  fibb(x-1) + fibb(x-2)
+fibb :: (Integer,Integer,Integer) -> Integer -> Integer
+fibb (curr, x, y) n = if (curr<n) then fibb (curr+1, y, y+x) n else y
+-- здесь каждый член последовательности вычисляется один раз, что улучшает скорость работы до линейной O(n)
+-- нумерация чисел последовательности начинается с "0" - 0, 1, 2, ...
 
 -- 7 -----------------------------------
 -- Реализуйте рекурсивную функцию, которая определяет является ли палиндромом
